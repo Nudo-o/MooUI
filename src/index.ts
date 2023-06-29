5e710ebebeeadbb175088402b6a71cb1b887b1c0
Nudo <82189052+Nudo-o@users.noreply.github.com>
2023-06-29 11:09:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,72 @@
+import columnsTemplate from "./configs/templates/columns.ts"
+import ICreateMenu from "./interfaces/ICreateMenu.ts"
+import Menu from "./menu/Menu.ts"
+import Column from "./menu/column/Column.ts"
+import Checkbox from "./menu/column/models/Checkbox.ts"
+import OptionCheckbox from "./menu/column/models/option/OptionCheckbox.ts"
 
+const MooUI = (function() {
+    this.list = new Map()
+
+    this.Column = Column
+    this.Checkbox = Checkbox
+    this.OptionCheckbox = OptionCheckbox
+
+    this.createMenu = function({
+        toggleKey,
+        appendNode
+    }: ICreateMenu): Menu {
+        const id: number = this.list.size + 1
+        const menu: Menu = new Menu({
+            id: id,
+            config: columnsTemplate,
+            toggleKey: toggleKey,
+            appendNode: appendNode
+        })
+
+        menu.destroy = () => {
+            menu.holder.remove()
+
+            this.list.delete(menu.id)
+        }
+
+        return this.list.set(id, menu).get(id)
+    }
+
+    this.eachAllMenu = function(
+        callback: (menu: Menu) => unknown,
+        predicate?: (menu: Menu) => unknown
+    ): void {
+        typeof predicate === 'undefined' && (predicate = () => true)
+
+        Array.from(this.list.values()).filter(
+            (item: any) => item.isMenu && predicate(item)
+        ).forEach(
+            (menu: any) => callback(menu)
+        )
+    }
+
+    this.toggleAllMenu = function(action: ("show" | "hide")): void {
+        this.eachAllMenu((menu: any) => {
+            menu[action]()
+        }, (menu: any) => action === "show" ? !menu.isVisible : menu.isVisible)
+    }
+
+    this.showAllMenu = function(): void {
+        this.toggleAllMenu("show")
+    }
+
+    this.hideAllMenu = function(): void {
+        this.toggleAllMenu("hide")
+    }
+
+    console.log("MooUI.js v1.0.0")
+    
+    return this
+}).call(Object.create({}))
+
+declare namespace window {
+    let MooUI: any
+}
+
+window.MooUI = MooUI
