5e627b893e3d9fe10432ce7d13c210db9ee3a840
Nudo <82189052+Nudo-o@users.noreply.github.com>
2023-07-03 03:15:06 +0300

    Update OptionCheckbox.ts

--- src/menu/column/models/option/OptionCheckbox.ts
+++ src/menu/column/models/option/OptionCheckbox.ts
@@ -1,12 +1,13 @@
 import IOptionCheckbox from "../../../../interfaces/checkbox/IOptionCheckbox"
 import Option from "./Option"
 
-
 class OptionCheckbox extends Option {
     public isActive: IOptionCheckbox["isActive"]
     public isDisabled: IOptionCheckbox["isDisabled"]
 
     public element: HTMLElement
+
+    private events: Map<string, Map<number, Function>>
 
     constructor({ 
         key, 
@@ -15,7 +16,7 @@ class OptionCheckbox extends Option {
         isActive = false,
         isDisabled = false
     }: IOptionCheckbox) {
-        const element = document.createElement("box")
+        const element: HTMLElement = document.createElement("box")
 
         super({ key, name, description, 
             node: element
@@ -25,6 +26,10 @@ class OptionCheckbox extends Option {
         this.isDisabled = isDisabled
 
         this.element = element
+
+        this.events = new Map([
+            [ "click", new Map() ]
+        ])
     }
 
     private get html(): string {
@@ -33,13 +38,82 @@ class OptionCheckbox extends Option {
         `
     }
 
+    public on(event: string, callback: Function) {
+        const eventMap: Map<number, Function> = this.events.get(event)
+
+        eventMap.set(eventMap.size + 1, callback)
+    }
+
+    public setActive(activeState: boolean): void {
+        this.isActive = activeState
+
+        this.updateClasses()
+    }
+
+    public setDisabled(activeState: boolean): void {
+        this.isDisabled = activeState
+
+        this.updateClasses()
+    }
+    
+    private toggle(): boolean {
+        this.setActive(!this.isActive)
+
+        const eventMap: Map<number, Function> = this.events.get("click")
+
+        eventMap.forEach((callback: Function) => {
+            callback(this.isActive, this.element)
+        })
+
+        return this.isActive
+    }
+
+    private initEvents(): void {
+        let mousePressed: boolean = false
+
+        this.element.addEventListener("mousedown", (event) => {
+            if (mousePressed) return
+
+            if (event.button === this.config.checkbox.toggleKey && !this.isDisabled) {
+                this.toggle()
+            }
+
+            mousePressed = true
+        })
+
+        window.addEventListener("mouseup", (event) => {
+            if (
+                event.button !== this.config.checkbox.toggleKey &&
+                event.button !== this.config.checkbox.openOptionsKey
+            ) return
+
+            mousePressed = false
+        })
+    }
+
     private updateClasses(): void {
         const activeClass = (isActive: boolean): string => isActive ? "active" : "inactive"
         
         this.element.classList.remove(activeClass(!this.isActive))
         this.element.classList.add(activeClass(this.isActive))
 
-        this.isDisabled && this.element.classList.add("disabled")
+        if (this.isDisabled) {
+            this.element.classList.add("disabled")
+        } else {
+            this.element.classList.remove("disabled")
+        }
+    }
+
+    public build(): void {
+        this.element.classList.add("ui-model", "checkbox-model")
+
+        this.updateClasses()
+
+        this.parent.appendChild(this.element)
+
+        this.element.insertAdjacentHTML("beforeend", this.html)
+
+        this.initEvents()
     }
 }
 
